3672b357b3525d35a54649312daa1055fdd394ec
Ramakm <itsramakrushna@gmail.com>
2020-11-27 15:11:23 +0000

    Files uploaded

---
+++ testing_python_apps/blog/test/unit/post_test.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from post import post
+
+class Post_Test(TestCase):
+    #have to write test in first then add other things in construction
+    def test_create_post(self):
+        p = post('Test', 'Test Content')
+        self.assertEqual('Test', p.title)
+        self.assertEqual('Test Content', p.content)
+
+
+    def test_json_post(self):
+        p = post('Test', 'Test Content')
+        expected = {'title': 'Test', 'content': 'Test Content'}
+        self.assertDictEqual(expected, p.json())
